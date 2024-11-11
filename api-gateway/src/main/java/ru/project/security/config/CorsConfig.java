281d558f7875d6d47a850d3cd927e044326c2b2e
COSMOGLIST <113556645+COSMOGLIST@users.noreply.github.com>
2024-11-12 00:30:35 +0300

    aboba

--- api-gateway/src/main/java/ru/project/security/config/CorsConfig.java
+++ api-gateway/src/main/java/ru/project/security/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedOrigins(List.of("*"));
